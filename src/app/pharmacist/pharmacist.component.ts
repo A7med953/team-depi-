d39ed3056c945864dfecaa1f0fc0014c1293a53d
ahmed elsayed <a7md1elsayed@gmail.com>
2025-10-11 21:45:25 +0300

    first commit

---
+++ src/app/pharmacist/pharmacist.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pharmacist',
+  imports: [],
+  templateUrl: './pharmacist.component.html',
+  styleUrl: './pharmacist.component.css'
+})
+export class PharmacistComponent {
+
+}
