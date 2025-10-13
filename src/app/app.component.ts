8ef9a34fcc2642e0d7c9c668d3394d8d0271901c
ahmed elsayed <a7md1elsayed@gmail.com>
2025-10-13 18:42:08 +0300

    13/10by ahmed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { AppointmentsComponent } from './patient/appointments/appointments.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AppointmentsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
