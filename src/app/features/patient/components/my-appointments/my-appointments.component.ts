7ee76852c07a8584f632c90a7ea6e8bb76d7be72
Abdallah <ballaabotaleb@gmail.com>
2025-10-15 23:29:48 +0300

    Finished my-appointments component

--- src/app/features/patient/components/my-appointments/my-appointments.component.ts
+++ src/app/features/patient/components/my-appointments/my-appointments.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-my-appointments',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './my-appointments.component.html',
   styleUrl: './my-appointments.component.css'
 })
-export class MyAppointmentsComponent {
 
+export class MyAppointmentsComponent implements OnInit {
+
+  appointments: {
+    date: string;
+    timeStart: string;
+    timeEnd: string;
+    doctor: string;
+    status: string;
+    reason: string;
+  }[] = [
+    { date: '2024-07-01', timeStart: '10:00 AM', timeEnd: '10:30 AM', doctor: 'Dr. Smith', status: 'Confirmed', reason: "Chest pain" },
+    { date: '2024-07-03', timeStart: '10:00 AM', timeEnd: '10:30 AM', doctor: 'Dr. Mark', status: 'Confirmed', reason: "Chest pain" },
+  ];
+
+  pastAppointments: {
+    date: string;
+    timeStart: string;
+    timeEnd: string;
+    doctor: string;
+    status: string;
+    reason: string;
+  }[]  = [
+];
+
+  ngOnInit() {
+    console.log('Component initialized');
+    console.log('Appointments:', this.appointments);
+    console.log('Past Appointments:', this.pastAppointments);
+  }
 }
