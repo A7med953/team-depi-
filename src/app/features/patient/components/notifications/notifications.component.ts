eb863f6232cd6c34938f0cfd869adc4837cc1557
Heba Kamar <hebaamar1392004@gmail.com>
2025-11-14 06:09:40 -0800

    find-hospital, my-profile and notifications done

--- src/app/features/patient/components/notifications/notifications.component.ts
+++ src/app/features/patient/components/notifications/notifications.component.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
+import { not } from 'rxjs/internal/util/not';
+import { NgIf } from '@angular/common';
+import { NgFor } from '@angular/common';  
 
 @Component({
   selector: 'app-notifications',
-  imports: [],
+  imports: [NgIf, NgFor],
   templateUrl: './notifications.component.html',
-  styleUrl: './notifications.component.css'
+  styleUrls: ['./notifications.component.css'],
 })
 export class NotificationsComponent {
+  notifications = [
+    {
+      title: 'Appointment Reminder',
+      message: 'You have an appointment with Dr. Smith tomorrow at 10:00 AM.',
+      time: 'Just now',
+      icon: 'fa-regular fa-calendar',
+      read: false
+    },
+    {
+      title: 'Prescription Ready',
+      message: 'Your prescription for Lisinopril is ready for pickup at City Pharmacy.',
+      time: '2h ago',
+      icon: 'fa-solid fa-capsules',
+      read: false
+    }
+  ];
+
+  clearNotification(index: number): void {
+    this.notifications.splice(index, 1);
+  }
+
+  markAsRead(index: number): void {
+    this.notifications[index].read = true;
+  }
 
 }
